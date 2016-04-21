8c3122872ee3cb7f6e364380c18f94f0cf570401
BurnPizza <xburntpizza@gmail.com>
2016-04-21 13:54:27 -0400

    Use bytecode compilation

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -6,14 +6,17 @@ use std::error::Error;
 use reader::{self, Form, InputStream};
 use analyzer::{self, Analysis, AstNode};
 use symbol_table::*;
+use vm::*;
+
 
 // TODO: return type
 pub fn interpret<T: Into<String>>(input: T) -> String {
     let input = input.into();
     let form = read(input).unwrap();
     let analysis = analyze(form).unwrap();
+    let program = compile(analysis).unwrap();
 
-    exec_and_print(analysis)
+    exec(program)
 
     //    read(input)
     //        .and_then(analyze)
@@ -31,6 +34,93 @@ fn read(input: String) -> Result<Vec<Form>, Box<Error>> {
 }
 fn analyze(input: Vec<Form>) -> Result<Analysis, Box<Error>> {
     analyzer::analyze_from_root(input).map_err(Into::into)
+}
+
+fn compile(input: Analysis) -> Result<Program, Box<Error>> {
+    fn emit_const(p: &mut ProgramBuilder, val: i64) -> Result<(), Box<Error>> {
+        p.load_const(val);
+        Ok(())
+    }
+
+    fn emit_var(p: &mut ProgramBuilder, var_id: u32, env: &SymbolTable) -> Result<(), Box<Error>> {
+        let var_record = env.lookup_id(var_id).unwrap();
+
+        unimplemented!()
+    }
+
+    fn emit_do(p: &mut ProgramBuilder,
+               env: &SymbolTable,
+               args: &[AstNode])
+               -> Result<(), Box<Error>> {
+        let (last, others) = args.split_last().expect("emit_do");
+
+        for arg in others {
+            try!(emit(arg, env, p));
+            p.drop();
+        }
+        emit(last, env, p)
+    }
+
+    fn emit_inv(p: &mut ProgramBuilder,
+                callee: &AstNode,
+                args: &[AstNode],
+                env: &SymbolTable)
+                -> Result<(), Box<Error>> {
+
+
+        match *callee {
+            AstNode::Var(id) => {
+                let var_record = env.lookup_id(id).unwrap();
+                match *var_record.kind() {
+                    VarKind::Normal => {
+                        for arg in args {
+                            try!(emit(arg, env, p));
+                        }
+                        emit(callee, env, p)
+                    }
+                    VarKind::Special(_) => {
+                        match var_record.ident() {
+                            "do" => emit_do(p, env, args),
+                            "+" => {
+                                for arg in args {
+                                    try!(emit(arg, env, p));
+                                }
+                                p.add(args.len());
+                                Ok(())
+                            } 
+                            _ => unimplemented!(),
+                        }
+                    }
+                }
+            }
+            _ => {
+                for arg in args {
+                    try!(emit(arg, env, p));
+                }
+                emit(callee, env, p)
+            }
+        }
+    }
+    fn emit(node: &AstNode, env: &SymbolTable, p: &mut ProgramBuilder) -> Result<(), Box<Error>> {
+        match *node {
+            AstNode::Const(val) => emit_const(p, val),
+            AstNode::Var(id) => emit_var(p, id, env),
+            AstNode::Inv(ref callee, ref args) => emit_inv(p, callee, args, env),
+        }
+    }
+
+    let mut program = new_program();
+    try!(emit(input.ast(), input.env(), &mut program));
+    program.exit();
+
+    let program = program.finish();
+
+    println!("Program: {:?}", program);
+    Ok(program)
+}
+
+fn exec(input: Program) -> String {
+    format!("{}", exec_program(input))
 }
 
 fn exec_and_print(analysis: Analysis) -> String {
--- src/vm.rs
+++ src/vm.rs
@@ -1,11 +1,18 @@
 
+use itertools::*;
 use byteorder::*;
 
-type RegisterT = i64;
+use std::fmt::{self, Debug, Formatter};
+
+pub type RegisterT = i64;
 
 
-fn exec_program(program: Program) -> RegisterT {
+pub fn exec_program(program: Program) -> RegisterT {
     use self::Instruction::*;
+
+    fn pop_usize(ds: &mut Vec<RegisterT>) -> usize {
+        ds.pop().expect("stack underflow") as usize
+    }
 
     let mut ip = 0; // instruction pointer
     let mut data_stack = Vec::with_capacity(128);
@@ -14,18 +21,34 @@ fn exec_program(program: Program) -> RegisterT {
     loop {
         let i = ip;
         ip += 1;
-        match as_inst(program.instructions[i]) {
+        let instr = as_inst(program.instructions[i]);
+        println!("idx {}: {:?}", i, instr);
+        match instr {
             LoadConst => {
                 let val = LittleEndian::read_i64(&program.instructions[ip..ip + 8]);
                 ip += 8;
                 data_stack.push(val);
             }
             Add => {
-                let num_args = data_stack.pop().expect("stack underflow: add") as usize;
+                let num_args = pop_usize(&mut data_stack);
                 assert!(data_stack.len() >= num_args, "stack underflow: add");
                 let idx = data_stack.len() - num_args;
                 let val = data_stack.drain(idx..).fold(0, |acc, e| acc + e);
                 data_stack.push(val);
+            }
+            Do => {
+                let num_args = pop_usize(&mut data_stack);
+                assert!(num_args > 0); // TODO
+                assert!(data_stack.len() >= num_args, "stack underflow: do");
+                let idx = data_stack.len() - num_args;
+                let val = data_stack.drain(idx..).fold(0, |_, e| {
+                    let ret = unimplemented!();
+                    ret
+                });
+                data_stack.push(val);
+            }
+            Drop => {
+                data_stack.pop().expect("stack underflow: drop");
             }
             CallSpecial => unimplemented!(),
             CallNormal => unimplemented!(),
@@ -42,40 +65,45 @@ fn as_inst(b: u8) -> Instruction {
     unsafe { ::std::mem::transmute(b) }
 }
 
-#[derive(Debug, PartialEq)]
-struct Program {
+pub struct Program {
     instructions: Vec<u8>,
 }
 
-fn new_program() -> ProgramBuilder {
+pub fn new_program() -> ProgramBuilder {
     ProgramBuilder { instructions: vec![] }
 }
 
-struct ProgramBuilder {
+pub struct ProgramBuilder {
     instructions: Vec<u8>,
 }
 
 impl ProgramBuilder {
-    fn load_const(mut self, val: RegisterT) -> Self {
+    pub fn load_const(&mut self, val: RegisterT) {
         self.instructions.push(as_byte(Instruction::LoadConst));
         let mut tmp = [0; 8];
         LittleEndian::write_i64(&mut tmp, val);
         self.instructions.extend_from_slice(&tmp);
-        self
     }
 
-    fn add(mut self, num_args: u8) -> Self {
-        self = self.load_const(num_args as i64);
+    pub fn add(&mut self, num_args: usize) {
+        self.load_const(num_args as i64);
         self.instructions.push(as_byte(Instruction::Add));
-        self
     }
 
-    fn exit(mut self) -> Self {
+    pub fn do_(&mut self, num_args: usize) {
+        self.load_const(num_args as i64);
+        self.instructions.push(as_byte(Instruction::Do));
+    }
+
+    pub fn drop(&mut self) {
+        self.instructions.push(as_byte(Instruction::Drop));
+    }
+
+    pub fn exit(&mut self) {
         self.instructions.push(as_byte(Instruction::Exit));
-        self
     }
 
-    fn finish(self) -> Program {
+    pub fn finish(self) -> Program {
         Program { instructions: self.instructions }
     }
 }
@@ -84,27 +112,97 @@ impl ProgramBuilder {
 enum Instruction {
     LoadConst,
     Add,
+    Do,
+    Drop,
     CallSpecial,
     CallNormal,
     Exit,
 }
 
+impl Debug for Program {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        use self::Instruction::*;
+
+        let lines = self.instructions
+                        .iter()
+                        .enumerate()
+                        .batching(|mut it| {
+                            match it.next() {
+                                None => None,
+                                Some((idx, &b)) => {
+                                    match as_inst(b) {
+                                        LoadConst => {
+                                            let mut raw = vec![];
+                                            for _ in 0..8 {
+                                                match it.next() {
+                                                    Some((_, &b)) => {
+                                                        raw.push(b);
+                                                    }
+                                                    _ => return None,
+                                                }
+                                            }
+                                            let val = LittleEndian::read_i64(&*raw);
+                                            Some(format!("{:>4}:\t ldc {:>3}", idx, val))
+                                        }
+                                        Add => Some(format!("{}:add ?", idx)),
+                                        Do => Some(format!("{}:do ?", idx)),
+                                        Exit => Some(format!("{:>4}:\texit", idx)),
+                                        Drop => Some(format!("{:>4}:\tdrop", idx)),
+                                        _ => unimplemented!(),
+                                    }
+                                }
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .into_iter()
+                        .rev()
+                        .batching(|mut it| {
+                            match it.next() {
+                                None => None,
+                                Some(string) => {
+                                    if string.contains("add") {
+                                        let next = it.next().unwrap();
+                                        Some(format!("{:>4}:\t add {:>3}",
+                                                     &next[..next.find(':').unwrap()],
+                                                     &next[next.rfind(' ').unwrap()..]))
+
+                                    } else if string.contains("do") {
+                                        let next = it.next().unwrap();
+                                        Some(format!("{:>4}:\t  do {:>3}",
+                                                     &next[..next.find(':').unwrap()],
+                                                     &next[next.rfind(' ').unwrap()..]))
+
+                                    } else {
+                                        Some(string)
+                                    }
+                                }
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .into_iter()
+                        .rev()
+                        .join("\n");
+
+        writeln!(f, "\n{}", lines)
+    }
+}
+
 #[cfg(test)]
 mod test {
-
-    use super::{new_program, exec_program};
+    use super::*;
 
     #[test]
     fn raw_program_addition() {
         // (+ 1 2 3) == 6
 
-        let p = new_program()
-                    .load_const(1)
-                    .load_const(2)
-                    .load_const(3)
-                    .add(3)
-                    .exit()
-                    .finish();
+        let mut p = new_program();
+        p.load_const(1);
+        p.load_const(2);
+        p.load_const(3);
+        p.add(3);
+        p.exit();
+
+        let p = p.finish();
 
         assert_eq!(exec_program(p), 6);
     }
